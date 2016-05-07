30741b75feb8ddbe362f722762bd44bdd1a2d64b
nazmul56 <nazmul.cste07@gmail.com>
2016-05-07 20:32:46 +0600

    Image Store in Local Folder from Internet

---
+++ app/src/main/java/com/mashpy/nstuinfo/DetaitlsActivity.java
@@ -0,0 +1,52 @@
+package com.mashpy.nstuinfo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+public class DetaitlsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detaitls);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        String Title= getIntent().getStringExtra("title");
+        toolbar.setTitle(Title);
+        setSupportActionBar(toolbar);
+
+
+        TextView URLtv = (TextView)findViewById(R.id.urltv);
+        WebView detatils_wv = (WebView)findViewById(R.id.webview);
+
+        detatils_wv.getSettings().setJavaScriptEnabled(true);
+        String UrlFrom_MainActivity= getIntent().getStringExtra("url");
+       // URLtv.setText(UrlFrom_MainActivity);
+
+       // detatils_wv.set
+
+        detatils_wv.loadDataWithBaseURL(null, UrlFrom_MainActivity, "text/html", "utf-8","about:blank");
+
+        URLtv.setText(Html.fromHtml(UrlFrom_MainActivity));
+
+        // Making url clickable
+        URLtv.setMovementMethod(LinkMovementMethod.getInstance());
+        URLtv.setVisibility(View.VISIBLE);
+
+      /**  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+      .setAction("Action", null).show();
+      }
+      });*/
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
--- app/src/main/java/com/mashpy/nstuinfo/MainActivity.java
+++ app/src/main/java/com/mashpy/nstuinfo/MainActivity.java
@@ -1,13 +1,14 @@
 package com.mashpy.nstuinfo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent i = new Intent(getApplicationContext(), ImageViewTest.class);
+                startActivity(i);
             }
         });
     }
