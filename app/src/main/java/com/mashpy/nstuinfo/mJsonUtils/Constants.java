f0b681f2ebd15c43efe18fd24e3ffd54bd321f9f
shhridoy <shhridoy.cste@gmail.com>
2018-11-16 21:36:10 +0600

    test version 4.0

--- app/src/main/java/com/mashpy/nstuinfo/mJsonUtils/Constants.java
+++ app/src/main/java/com/mashpy/nstuinfo/mJsonUtils/Constants.java
@@ -6,7 +6,7 @@ package com.mashpy.nstuinfo.mJsonUtils;
 
 public class Constants {
 
-    public static final String JSON_CHECKING_URL = "https://jsonblob.com/api/0397413c-e36b-11e8-aa48-9d4d39ad8074";
-    public static String JSON_DATA_URL; // = "https://jsonblob.com/api/6a1a5234-d30f-11e8-9c58-b1987dc5c254";
+    public static final String JSON_CHECKING_URL = "https://raw.githubusercontent.com/Mashpy/nstuinfo/master/version_4.json"; //"https://jsonblob.com/api/0397413c-e36b-11e8-aa48-9d4d39ad8074";
+    public static String JSON_DATA_URL;
 
 }
