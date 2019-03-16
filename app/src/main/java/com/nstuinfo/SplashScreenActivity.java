61e5ecc292e5ac82248a78341d25581b0e2b0a83
shhridoy <md.saifulhq@gmail.com>
2019-03-16 23:49:12 +0600

    added splash screen, animation and image

--- app/src/main/java/com/nstuinfo/SplashScreenActivity.java
+++ app/src/main/java/com/nstuinfo/SplashScreenActivity.java
@@ -4,15 +4,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.nstuinfo.mOtherUtils.AnimationUtils;
 
 public class SplashScreenActivity extends Activity {
 
     public static final int SPLASH_SCREEN_TIME = 2500;
+    private ImageView topImg, bottomImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        topImg = findViewById(R.id.topIM);
+        bottomImg = findViewById(R.id.bottomIM1);
+
+        AnimationUtils.shake(this, topImg);
+        AnimationUtils.shake(this, bottomImg);
 
         new Handler().postDelayed(new Runnable() {
             @Override
--- app/src/main/java/com/nstuinfo/mOtherUtils/AnimationUtils.java
+++ app/src/main/java/com/nstuinfo/mOtherUtils/AnimationUtils.java
@@ -2,9 +2,13 @@ package com.nstuinfo.mOtherUtils;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
+
+import com.nstuinfo.R;
 
 /**
  * Created by whoami on 10/16/2018.
@@ -34,6 +38,48 @@ public class AnimationUtils {
         animation.setDuration(duration);
         animation.setFillAfter(true);
         view.startAnimation(animation);
+    }
+
+    public static void shake(Context context, final View view) {
+
+        final Animation shake1 = android.view.animation.AnimationUtils.loadAnimation(context, R.anim.shake_1);
+        final Animation shake2 = android.view.animation.AnimationUtils.loadAnimation(context, R.anim.shake_2);
+
+        view.startAnimation(shake1);
+
+        shake1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                view.startAnimation(shake2);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        shake2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                view.startAnimation(shake1);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
 }
