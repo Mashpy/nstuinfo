69c72ff60e77920d449ededcd12cfd6b92e26be5
shhridoy <shhridoy.cste@gmail.com>
2019-07-07 20:21:41 +0600

    added network security for android upper versions

--- app/src/main/java/com/nstuinfo/mJsonUtils/Constants.java
+++ app/src/main/java/com/nstuinfo/mJsonUtils/Constants.java
@@ -6,8 +6,8 @@ package com.nstuinfo.mJsonUtils;
 
 public class Constants {
 
-    public static String JSON_CHECKING_URL = "http://nstudiary.nstu.edu.bd/checkdata.json";
-    public static String JSON_DATA_URL = "http://nstudiary.nstu.edu.bd/nstudiary.json";
+    public static String JSON_CHECKING_URL = "https://nstudiary.nstu.edu.bd/checkdata.json";
+    public static String JSON_DATA_URL = "https://nstudiary.nstu.edu.bd/nstudiary.json";
 
     //public static final String JSON_CHECKING_URL = "http://nstuinfo.rockertuhin.com/api/versionapi";
     //public static final String JSON_DATA_URL = "http://nstuinfo.rockertuhin.com/api/infoapi";
